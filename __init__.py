b849e1de454a000c7da530f2f98b2d73fb27db62
Austin Herman <austin.j.herman@gmail.com>
2016-08-26 00:19:27 -0400

    Second commit - connected to mysql, added author view

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__.split('.')[0])
 app.config.from_object('settings')
 
-from home import views
+db = SQLAlchemy(app)
+
+from blog import views
+from author import views
 
 
 
--- settings.py
+++ settings.py
@@ -1,2 +1,15 @@
+import os
+
 SECRET_KEY = '\x0c\xd8\xd8\xa9S\x9d\x93\xac5\xca\x15RX\xb1\t\x07\x0c\xd1E\x0b\xbf\x1c\x98\x18'
 DEBUG = True
+
+# Database configuration
+DB_USERNAME = 'austin'
+DB_PASSWORD = 'AH!4SMG'
+BLOG_DATABASE_NAME = 'flask_blog'
+DB_HOST = 'localhost'
+DB_URI = 'mysql+pymysql://%s:%s@%s/%s' % (DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
+
+# SQL ALCHEMY SETTINGS
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = True
